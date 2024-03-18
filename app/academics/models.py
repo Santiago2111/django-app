74384685605bfd630ef84656621b238ba2d922da
Santiago2111 <sasaproxd8@gmail.com>
2024-03-18 11:15:42 -0500

    18-03-24

--- app/academics/models.py
+++ app/academics/models.py
@@ -2,6 +2,14 @@ from django.db import models
 import datetime
 # Create your models here.
 class User(models.Model):
+    email = models.EmailField(null = True, blank = True, default = True)
+    password = models.CharField(null = True, blank = True, default = True)
+    status = models.BooleanField(null = True, blank = True, default = True)
+    created_at = models.DateTimeField(default=datetime.datetime.now())
+    updated_at = models.DateTimeField(default=datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+    
+class Person(models.Model):
     firstname = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20)
     age = models.IntegerField()
