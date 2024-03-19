32a4582e54f1c05ab65658be73a0ab1c748089c4
Santiago2111 <sasaproxd8@gmail.com>
2024-03-19 12:07:24 -0500

    19-03-24

--- app/academics/models.py
+++ app/academics/models.py
@@ -8,6 +8,8 @@ class User(models.Model):
     created_at = models.DateTimeField(default=datetime.datetime.now())
     updated_at = models.DateTimeField(default=datetime.datetime.now())
     deleted_at = models.DateTimeField(null = True, blank = True)
+    def __str__(self):
+        return self.email
     
 class Person(models.Model):
     firstname = models.CharField(max_length=20)
