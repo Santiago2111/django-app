24b2f145564cde5a5d817e8bb3d9ab85346437af
Santiago2111 <sasaproxd8@gmail.com>
2024-04-22 11:15:16 -0500

    22/04/2024

--- app/academics/views.py
+++ app/academics/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
 def index(request):
-    return HttpResponse("::: WELCOME TO MY SITE jorginho :::")
+    return HttpResponse("::: WELCOME TO MY SITE :::")
 
-def list_persons(request):
-    return HttpResponse(":::  :::")
+def list_users(request):
+    #return HttpResponse("Here you find a list of users")
+    users = User.objects.all()
+    return render(request, 'academics/list_user.html', {'users': users})
+
+def create_user(request):
+    return HttpResponse("Here you find a list of people")
